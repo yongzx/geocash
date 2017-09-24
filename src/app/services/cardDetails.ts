f3addf9afa739b3348f1f5603a98168878510f1e
yongzx <zhengxin.yong@minerva.kgi.edu>
2017-09-23 20:54:53 -0700

    Services

---
+++ src/app/services/cardDetails.ts
@@ -0,0 +1,13 @@
+export class cardDetails {
+  cardId: string;
+  displayCardNumber: string;
+  localCardActivationIndicator : string;
+  overseasCardActivationIndicator : string;
+  perpetualActivationFlag : boolean;
+  overseasCardActivationStartDate : string;
+  overseasCardActivationEndDate : string;
+  currentCreditLimitAmount : number;
+  subCardType : string;
+  cardHolderType : string;
+  cardIssueReason : string;
+}
